eada64c780e4fa7f82874a6ebc1e03a679af64d4
George Butter <threeninenineone@gmail.com>
2021-07-28 19:53:10 +1000

    feat: export layout components

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 export * from '@providers/Kaze';
 
+export * from '@layouts/AspectRatio/AspectRatio';
 export * from '@layouts/Box/Box';
 export * from '@layouts/Center/Center';
 export * from '@layouts/Cluster/Cluster';
 export * from '@layouts/Cover/Cover';
+export * from '@layouts/Grid/Grid';
 export * from '@layouts/HStack/HStack';
+export * from '@layouts/Reel/Reel';
 export * from '@layouts/Sidebar/Sidebar';
 export * from '@layouts/Sidebar/SidebarSide';
 export * from '@layouts/Sidebar/SidebarMain';
